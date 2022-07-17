8d48b863f2f6ae504132239a2be5902c657634bc
smartful <remi_rodrigues@hotmail.com>
2022-07-17 15:39:18 +0200

    calculer et afficher les totaux

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -3,7 +3,7 @@ import cartItems from "../../cartItems";
 
 const initialState = {
   cartItems: cartItems,
-  amount: 2,
+  amount: 0,
   total: 0,
   isLoading: true,
 };
@@ -27,8 +27,21 @@ const cartSlice = createSlice({
       const cartItem = state.cartItems.find((item) => item.id === payload.id);
       cartItem.amount -= 1;
     },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+
+      state.amount = amount;
+      state.total = total;
+    },
   },
 });
 
-export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions;
 export default cartSlice.reducer;
